aef239f2e2469bacd7ae5408a6696381109cc8e4
63vicky <6326vivekgangani@gmail.com>
2025-06-11 21:11:24 +0530

    feat: update title in index.html and configure CORS to include CLIENT_URL

--- Assignment-1/server/index.js
+++ Assignment-1/server/index.js
@@ -9,7 +9,15 @@ const connectDB = require('./config/db.js');
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'] }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      process.env.CLIENT_URL,
+    ],
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
